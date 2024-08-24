fe7507b0d90daacd7116f430f0800a92f2bed522
rishig1030 <guptarishi1030@gmail.com>
2024-08-25 00:55:53 +0530

    added about , potfolio and other section , with external CSS for responsive , and other images used in this site

--- script.js
+++ script.js
@@ -22,7 +22,12 @@ function pageTransistion(){
         })
     }
     //section active class
-    
+    // toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
     
 }
 
